0d8d5274a3ad2fac9e67fd50bda8bb60cf1a6e48
Prerit1221 <73111617+Prerit1221@users.noreply.github.com>
2025-03-02 17:52:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ provider "aws" {
     region = "eu-north-1"
 }
 
-resource "aws_s3_bucket" "firstbuckettttfffggg" {
-    bucket = "my-first-bucket-using-terraformmmmmmmm"
+resource "aws_s3_bucket" "firstbuckettttfffggg111" {
+    bucket = "my-first-bucket-using-terraformmmmmmmm1116969"
 
   tags = {
     Name        = "My S3"
@@ -24,22 +24,22 @@ lifecycle {
   
 }
 
-resource "aws_instance" "my_instanceeeee" {
+resource "aws_instance" "my_instanceeeee111" {
   ami                    = "ami-075449515af5df0d1" # Amazon Linux 2 AMI for eu-north-1
   instance_type          = "t3.micro"
 
   tags = {
-    Name = "ec2_temp"
+    Name = "ec2_temp1111"
     Environment = "test"
   }
 }
 
-resource "aws_instance" "my_instance11111" {
+resource "aws_instance" "my_instance11111444" {
   ami                    = "ami-075449515af5df0d1" # Amazon Linux 2 AMI for eu-north-1
   instance_type          = "t3.micro"
 
   tags = {
-    Name = "ec2_main"
+    Name = "ec2_main6969"
     Environment = "hello21"
   }
 }
